e8c1b231ada47b5a8c15a710e89032d28c2e4dd0
Vincenzo Colluto <v.colluto@iisvolta.it>
2023-02-17 19:07:29 +0100

    creazione classe Lavatrice

--- Lavanderia/src/it/edu/iisvolta/Lavatrice.java
+++ Lavanderia/src/it/edu/iisvolta/Lavatrice.java
@@ -1,5 +1,31 @@
 package it.edu.iisvolta;
 
-public class Lavatrice {
+import java.time.LocalTime;
 
+public class Lavatrice {
+	private int capacità;
+	private int livelloAcqua;
+	private int temperatura;
+	private int velocitàRotazione;
+	private boolean detersivo;
+	private int stato;
+	private float credito;
+	private LocalTime dataOraAvvio;
+	
+	public Lavatrice(int capacità) {
+		super();
+		this.capacità = capacità;
+		
+		//inizializzazione attributi
+		livelloAcqua=0;
+		temperatura=0;
+		velocitàRotazione=0;
+		stato=0;
+		credito= 0;
+		detersivo= false;
+	}
+	
+	
+	
+	
 }
